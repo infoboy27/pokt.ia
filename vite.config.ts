2b402176b0ecda53dd64f651d0f79cb45552407b
Jonatan <jonatan.maria@weaversnodes.com>
2025-08-01 20:18:35 -0400

    fix: Remove all Remix imports from vite.config.ts to resolve module issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import { vitePlugin as remix } from "@remix-run/dev"
-import { installGlobals } from "@remix-run/node"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
-
-installGlobals()
 
 export default defineConfig({
   server: {
     port: 3001,
   },
   plugins: [
-    remix(),
     tsconfigPaths(),
   ],
   ssr: {
